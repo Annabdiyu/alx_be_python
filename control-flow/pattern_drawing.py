a255756f6e9b5d11fcdd1c8138be89e1c48a2177
Anna Abdiyu <annaply156@gmail.com>
2024-09-21 20:27:34 +0300

    fourth commit

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,12 @@
 size=int(input("Enter the size of the pattern: "))
 n=1
 while n<=size:
-    j=1
-    while j<=size:
+    
+    """while j<=size:
         print("*",end="")
-        j+=1
+        j+=1"""
+    for j in range(size):
+        print("*",end="")
     print()
     n+=1
 
