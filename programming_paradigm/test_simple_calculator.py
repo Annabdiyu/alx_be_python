0fc8de45d72428ca18ec48e463aef498a8a40ab0
Anna Abdiyu <annaply156@gmail.com>
2024-10-05 21:03:38 +0300

    third commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,23 +4,29 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        """Set up the SimpleCalculator instance before each test."""
+
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        """Test the addition method."""
+
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-        # Add more assertions to thoroughly test the add method.
-    def test_substraction(self):
-        self.asserEqual(self.calc.substract(6,1),5)
-        self.assertEqual(self.calc.substract(99,90),9)
+        self.assertEqual(self.calc.add(5,7),12)
+    
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(6,1),5)
+        self.assertEqual(self.calc.subtract(99,90),9)
+        self.assertEqual(self.calc.subtract(1,1),0)
+
     def test_multiplicaiton(self):
         self.assertEqual(self.calc.multiply(10,0),0)
         self.assertEqual(self.calc.multiply(5,6),30)
+        self.assertEqual(self.calc.multiply(5,11),55)
+
     def test_division(self):
         self.assertEqual(self.calc.divide(10,0),None)
         self.assertEqual(self.calc.divide(100,10),10)
+        self.assertEqual(self.calc.divide(0,100),0)
 
 if __name__=='__main__':
     unittest.main()
